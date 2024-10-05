60aeb57766dedcd69dd2f99174cf76225e03a533
Дарья Юдакова <facemag1234@gmail.com>
2024-10-06 02:27:06 +0300

    time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -146,3 +146,42 @@ int get_seconds(int t)
 {
     return t % 60;
 }
+
+
+double time_to_utc(int utc_offset, double time)
+{
+    double utc_time = time - utc_offset;
+
+    if (utc_time < 0) {
+        utc_time += 24;
+    }
+    else if (utc_time >= 24) {
+        utc_time -= 24;
+    }
+
+    return utc_time;
+    /*
+        Return time at UTC+0, where utc_offset is the number of hours away from
+        UTC+0.
+        You may be interested in:
+        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+
+        >>> time_to_utc(+0, 12.0)
+        12.0
+
+        >>> time_to_utc(+1, 12.0)
+        11.0
+
+        >>> time_to_utc(-1, 12.0)
+        13.0
+
+        >>> time_to_utc(-11, 18.0)
+        5.0
+
+        >>> time_to_utc(-1, 0.0)
+        1.0
+
+        >>> time_to_utc(-1, 23.0)
+        0.0
+    */
+}
